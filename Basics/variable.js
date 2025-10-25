5970e1b033079ebef16e75e6f6a6209603dd4cc4
Bhavesh <bhaveshbhadane05@gmail.com>
2025-10-25 06:50:13 +0000

    just testing some issues

--- Basics/variable.js
+++ Basics/variable.js
@@ -1,11 +1,11 @@
 //suppose we are getting random registration details
 const accountID = 12345
-let accounEmail = "bhavesh@gmail.com"
+let accounEmail = "bhavesh_p@gmail.com"
 var accounPassword = "p1128e"
 accounCity = "Dhule"
 let accountUsername;
 
 //const id for things to be specefic
-// var is type of old use , generally we follow let due to some f
+// var is type of old use , generally we follow let due to some isuues
 //we can update let values
 console.table([accountID,accounEmail,accounPassword,accounCity,accountUsername])
