d196f81413a245f0f25bef918bf206ba12f222ac
Bhavesh <bhaveshbhadane05@gmail.com>
2025-10-25 06:31:10 +0000

    Add account object and fix console.table output

---
+++ Basics/variable.js
@@ -0,0 +1,11 @@
+//suppose we are getting random registration details
+const accountID = 12345
+let accounEmail = "bhavesh@gmail.com"
+var accounPassword = "p1128e"
+accounCity = "Dhule"
+let accountUsername;
+
+//const id for things to be specefic
+// var is type of old use , generally we follow let due to some f
+//we can update let values
+console.table([accountID,accounEmail,accounPassword,accounCity,accountUsername])
