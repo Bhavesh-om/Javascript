6e915861f8481a382e4e043f1c24b475ccab6b23
Bhavesh <bhaveshbhadane05@gmail.com>
2025-10-27 07:56:54 +0000

    updated conversion & operation

---
+++ Basics/conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33"
+console.log(typeof score)
+
+//conversion
+let valueinNumber= Number(score)
+console.log(valueinNumber)
+console.log(typeof valueinNumber)
+
+
+//"33" => 33
+// "33ab" => NaN
+// true => 1; lly 0 for f
+// "" => false
+//  "anything" => false
+
+//oprations
+//console.log(2+2)
+//lly modulo , sub, mul, div, etc.
+
+let str1 = "o"
+let str2 = " p"
+let str3 = str1 + str2
+console.log(str3);
+
+
+//observation
+console.log("1"+ 2)
+console.log( 1 + "2")
+console.log("1" + 1+2)
+console.log(2+1+ "2")
+//by observation we saw operation follows the opening datatype for whole expression although further we mixed up
+
+
+//some operation also as increment & decrement .
