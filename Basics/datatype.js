312ac98febfa253cb5e12ce8e8c4b703271e0beb
Bhavesh <bhaveshbhadane05@gmail.com>
2025-10-27 06:20:26 +0000

    Datatype updated

---
+++ Basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //good practice too perform modern js
+
+// alert(1+3) 
+// //alert works to pop up simply on web dev console
+//it has diff method to call by using node
+
+let name ="Om" //string
+let marks = 96 //number int or big int
+let ispassed = true // boolean
+
+//number => 2^33 limit
+//bigint
+//string => ""
+//bollean => true/false
+//null => datatype also standalone value
+//symbol => unique
+//object
+//note: undefined treated as datatype undefined
+//note: null is treated as datatype object
+console.log(typeof "om")
+
